4616229466668df1b1a1dc4ba5b12fbb6a5b578e
lisa chen <conglvse@gmail.com>
2017-11-06 13:53:20 +0800

    组件

---
+++ Demo1/HelloWorld.vue
@@ -0,0 +1,80 @@
+<!--
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <Counter v-bind:num="num"></Counter>--><!-- 调用组件 v-bind 绑定动态变量 --><!-- 
+    <p>父组件：{{num}}</p>
+  </div>
+</template>
+
+
+<script>
+import Counter from './conuter' //导入组件
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      num:10,
+      msg: 'Welcome '
+    }
+  },
+  components:{//注册局部组件
+    Counter //加载组件
+  }
+}
+</script>
+-->
+
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <Counter v-bind:num="num" v-on:dec="decrement" @incre="increment"></Counter><!-- dec 自定义事件-->
+    <p>父组件：{{num}}</p>
+  </div>
+</template>
+
+<script>
+import Counter from './conuter'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      num:10,
+      msg: 'Welcome '
+    }
+  },
+  components:{//注册局部组件
+    Counter
+  },
+  methods:{
+    decrement(){
+      this.num--;
+    },
+    increment(){
+      this.num++;
+    }
+  }
+}
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ Demo1/conuter.vue
@@ -0,0 +1,56 @@
+<!--
+<template>
+    <div>
+        <button @click="decrement">-</button>
+        <button @click="increment">+</button>
+        <p>子组件：{{num}}</p>
+    </div>
+</template>
+
+<script>
+    export default{
+        props:['num'],//父组件的数据需要通过 prop 才能下发到子组件中
+        data(){
+            return{
+                
+            }
+        },
+        methods:{
+            decrement(){
+              this.num--;
+            },
+            increment(){
+              this.num++;
+            }
+        }
+    }
+</script>
+-->
+
+<template>
+    <div>
+        <button @click="decrement">-</button>
+        <button @click="increment">+</button>
+        <p>子组件：{{num}}</p>
+    </div>
+</template>
+
+<script>
+    export default{
+        props:['num'],//父组件的数据需要通过 prop 才能下发到子组件中
+        data(){
+            return{
+                //num:0,
+                msg:'hello vue'
+            }
+        },
+        methods:{
+            decrement(){
+              this.$emit('dec');//子组件往父组件里传数据通过`emit()`触发，通过emit来触发父组件的方法，相当于jquery里的trigger， 复杂项目用Vuex
+            },
+            increment(){
+              this.$emit('incre');
+            }
+        }
+    }
+</script>
