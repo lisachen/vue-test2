fd5db644d54370d4c9cf7926e753131955ee5d4c
lisa chen <conglvse@gmail.com>
2017-11-09 11:17:57 +0800

    商品页面组件化拆分

---
+++ testMall/src/views/GoodList.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+        <header-component/>
+        <breadcrumb-component/>
+        <div class="accessory-result-page accessory-page">
+          <div class="container">
+            <div class="filter-nav">
+              <span class="sortby">Sort by:</span>
+              <a href="javascript:void(0)" class="default cur">Default</a>
+              <a href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
+              <a href="javascript:void(0)" class="filterby stopPop">Filter by</a>
+            </div>
+            <div class="accessory-result">
+              <!-- filter -->
+              <div class="filter stopPop" id="filter">
+                <dl class="filter-price">
+                  <dt>Price:</dt>
+                  <dd><a href="javascript:void(0)">All</a></dd>
+                  <dd>
+                    <a href="javascript:void(0)">0 - 100</a>
+                  </dd>
+                </dl>
+              </div>
+
+              <!-- search result accessories list -->
+              <div class="accessory-list-wrap">
+                <div class="accessory-list col-4">
+                  <ul>
+                    <li>
+                      <div class="pic">
+                        <a href="#"><img src="static/images/1.jpg" alt=""></a>
+                      </div>
+                      <div class="main">
+                        <div class="name">XX</div>
+                        <div class="price">XX</div>
+                        <div class="btn-area">
+                          <a href="javascript:;" class="btn btn--m">加入购物车</a>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <footer-component/>
+    </div>
+</template>
+<script>
+    import HeaderComponent from './../components/Header'
+    import BreadcrumbComponent from './../components/Breadcrumb'
+    import FooterComponent from './../components/Footer'
+    export default{
+        data(){
+            return{
+                msg:'hello vue'
+            }
+        },
+        components:{
+            HeaderComponent,
+            BreadcrumbComponent,
+            FooterComponent,
+        }
+    }
+</script>
